91ea90892c87dfd0c10433b03b948d4673651a36
Devon Church <notifydevon@gmail.com>
2020-08-06 11:10:15 +1200

    refactor: 💡 swap parcel domElement to use Vue $refs

--- content/src/App.editor.vue
+++ content/src/App.editor.vue
@@ -4,14 +4,18 @@ const ExplorerMedia = () => import('explorer/Media');
 
 export default {
     async mounted() {
-        const parcelProps = {
-            domElement: document.getElementById('single-spa-parcel:explorer-media')
-        };
+        const domElement = this.$refs[this.explorerMediaParcel];
+        const parcelProps = { domElement };
         const parcelConfig = await ExplorerMedia();
         this.explorerMediaParcel = mountRootParcel(parcelConfig, parcelProps);
     },
     async beforeDestroy() {
         await this.explorerMediaParcel.unmount();
+    },
+    data: function() {
+        return {
+            explorerMediaParcel: 'single-spa-parcel:explorer-media'
+        }
     }
 }
 </script>
@@ -19,6 +23,6 @@ export default {
 <template>
     <div>
         <h2>Vue | Content Editor (Dashboard)</h2>
-        <div id="single-spa-parcel:explorer-media"></div>
+        <div :ref="explorerMediaParcel"></div>
     </div>
 </template>
